1b593d6bb8b9cb01fd892830ee4526d344a47c22
kirangond <kirangond05@gmail.com>
2024-04-10 22:34:58 +0530

    comparator

---
+++ src/com/comparator/A.java
@@ -0,0 +1,20 @@
+package com.comparator;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String x = "xyz";
+		String y = "abc";
+		
+		//in sorting if obj1 comes first then it will return +ve value
+		System.out.println(x.compareTo(y));
+		
+		//if obj2 comes first then it will return -ve value
+		System.out.println(y.compareTo(x));
+
+
+	}
+
+}
